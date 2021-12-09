bc431c9a33100b229fbbd53aed96edfbc6aefbf4
Daniël de Kok <me@danieldk.eu>
2021-12-09 10:42:14 +0100

    Add tests for current ops

---
+++ thinc_sleef_ops/tests/test_ops.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pytest
+
+import thinc_sleef_ops
+
+
+@pytest.fixture
+def ops():
+    return thinc_sleef_ops.ArrayOps()
+
+
+def test_exp(ops):
+    a = np.arange(-3, 3.5, 0.5, dtype=np.float32)
+    assert np.allclose(ops.exp(a), np.exp(a))
+
+
+def test_erff(ops):
+    a = np.arange(-3, 3.5, 0.5, dtype=np.float32)
+    assert np.allclose(
+        ops.erf(a),
+        np.array(
+            [
+                -1.0000,
+                -0.9996,
+                -0.9953,
+                -0.9661,
+                -0.8427,
+                -0.5205,
+                0.0000,
+                0.5205,
+                0.8427,
+                0.9661,
+                0.9953,
+                0.9996,
+                1.0000,
+            ]
+        ),
+        atol=1e-4,
+    )
