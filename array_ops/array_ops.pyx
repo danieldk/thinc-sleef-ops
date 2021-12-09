c9654142984b87ac64ff4fa286b8242384a79f2c
Daniël de Kok <me@danieldk.eu>
2021-12-09 20:20:32 +0100

    Select array implementation based on CPU features

--- array_ops/array_ops.pyx
+++ array_ops/array_ops.pyx
@@ -2,7 +2,25 @@ from cython.operator cimport dereference as deref
 
 cdef class ArrayOps:
     def __init__(self):
-        self.array.reset(new Array[SSE]())
+        self.array.reset(new Array[Scalar]())
+        if cpuid_present() == 0:
+            return
+
+        cdef cpu_raw_data_t cpu_raw_data
+        cdef cpu_id_t cpu_id
+
+        if cpuid_get_raw_data(&cpu_raw_data) < 0:
+            return
+
+        if cpu_identify(&cpu_raw_data, &cpu_id) < 0:
+            return
+
+        if cpu_id.flags[int(CPU_FEATURE_AVX512F)] == 1:
+            self.array.reset(new Array[SSE]())
+        elif cpu_id.flags[int(CPU_FEATURE_AVX)] == 1:
+            self.array.reset(new Array[AVX]())
+        elif cpu_id.flags[int(CPU_FEATURE_SSE)] == 1:
+            self.array.reset(new Array[SSE]())
 
     def erf(self, a: float[:], in_place: bool=False):
         if not in_place:
